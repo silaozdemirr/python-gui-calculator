3d9ce573a9a98b8b8513c9aa3a95d87a0f5498d4
sıla <silaozdemir7532@gmail.com>
2026-07-05 19:42:04 +0300

    Create calculator window

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("GUI Calculator")
+import tkinter as tk
+
+window = tk.Tk()
+
+window.title("Calculator")
+
+window.geometry("320x450")
+
+window.resizable(False, False)
+
+window.mainloop()
+
