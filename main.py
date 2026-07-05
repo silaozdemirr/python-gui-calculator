a38f32c036dedcb7d1c987c2c62a8c97c4423855
sıla <silaozdemir7532@gmail.com>
2026-07-05 19:47:46 +0300

    Add calculator display

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ window.geometry("320x450")
 
 window.resizable(False, False)
 
-window.mainloop()
+display = tk.Entry(
+    window,
+    font=("Arial", 24),
+    justify="right",
+    bd=10
+)
 
+display.pack(fill="both", padx=10, pady=10, ipady=10)
+
+window.mainloop()
